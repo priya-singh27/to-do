f84a85683a95b00e67bfd51a44d4f9d4e3b74ef8
priya-singh27 <priya.beingdev@gmail.com>
2025-03-03 11:09:34 +0530

    Login.jsx changed

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -18,7 +18,7 @@ export default function Login({ email, password, handleEmailChange, handlePasswo
                 }),
                 {
                     headers: {
-                        "Content-Type": "application/json"
+                        "Content-Type": "application/x-www-form-urlencoded"
                     }
                 }
             );
